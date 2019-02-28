b457b86e3146314f3675a12284dfb13a00c9f8f5
Christopher Stura <christopher.stura@cloudreach.com>
2019-02-28 10:58:33 +0000

    added docs.php placeholder

--- _projectCommon.php
+++ _projectCommon.php
@@ -18,6 +18,6 @@
   # Define your project-wide Nav bars here.
   # Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank).
   $Nav->addNavSeparator("Eclipse Jemo",   "/jemo/");
-  $Nav->addCustomNav("Documentation", "/eclipse.org-common/themes/solstice/docs/", "_self", NULL);
+  $Nav->addCustomNav("Documentation", "/jemo/docs.php", "_self", NULL);
   $Nav->addCustomNav("Source code", "http://git.eclipse.org/c/www.eclipse.org/eclipse.org-common.git/tree/themes/solstice/", "_self", NULL);
   $Nav->addCustomNav("Using Phoenix", "http://wiki.eclipse.org/Using_Phoenix", "_self", NULL);
